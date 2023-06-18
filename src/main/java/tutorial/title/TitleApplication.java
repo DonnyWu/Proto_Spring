8640b9d595b9297b02d8ef194aebfaf308929e46
DonnyWu <Donny.wu4@gmail.com>
2023-06-18 01:01:24 -0400

    initial commit

---
+++ src/main/java/tutorial/title/TitleApplication.java
@@ -0,0 +1,13 @@
+package tutorial.title;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TitleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TitleApplication.class, args);
+	}
+
+}
