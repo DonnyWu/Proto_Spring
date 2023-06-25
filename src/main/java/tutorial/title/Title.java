4007a0591294e5b626269714d61b72c4749accad
DonnyWu <Donny.wu4@gmail.com>
2023-06-25 01:53:59 -0400

    Added Title and Review Class

---
+++ src/main/java/tutorial/title/Title.java
@@ -0,0 +1,31 @@
+package tutorial.title;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
+
+import java.util.List;
+
+@Document(collection = "title")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Title {
+    @Id
+
+    private ObjectId id;
+    private String imdb;
+    private String Title;
+    private String releaseDate;
+    private String trailerLink;
+    private String poster;
+    private List<String> genres;
+    private List<String> backdrops;
+    @DocumentReference
+    private List<Review> reviewIds;
+}
